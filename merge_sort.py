411d4abb9babc7084767486a989d3f9ba10871b5
jsnelling <jon.snelling@gmail.com>
2021-06-05 07:53:38 -0700

    migrate to pytest

--- merge_sort.py
+++ merge_sort.py
@@ -59,88 +59,91 @@ def merge(left, right):
       result.append(right[idx_right])
       idx_right += 1
 
-class TestStringMethods(unittest.TestCase):
+### Tests --------------------------------------------------------------------
+#-- Numeric Sort -------------------------------------------------------------
+def test_numeric_with_one_value():
+  input = [1]
+  expected = [1]
 
-  #-- Numeric Sort -------------------------------------------------------------
-  def test_numeric_with_one_value(self):
-    input = [1]
-    expected = [1]
+  assert merge_sort(input) == expected
 
-    self.assertEqual(merge_sort(input), expected)
+def test_numeric_with_two_values():
+  input = [1, 2]
+  expected = [1, 2]
 
-  def test_numeric_with_two_values(self):
-    input = [1, 2]
-    expected = [1, 2]
+  assert merge_sort(input) == expected
 
-    self.assertEqual(merge_sort(input), expected)
+def test_numeric_with_two_values_in_reverse_order():
+  input = [2, 1]
+  expected = [1, 2]
 
-  def test_numeric_with_two_values_in_reverse_order(self):
-    input = [2, 1]
-    expected = [1, 2]
+  assert merge_sort(input) == expected
 
-    self.assertEqual(merge_sort(input), expected)
+def test_numeric_with_even_number_of_values():
+  input = [2, 3, 1, 4]
+  expected = [1, 2, 3, 4]
 
-  def test_numeric_with_even_number_of_values(self):
-    input = [2, 3, 1, 4]
-    expected = [1, 2, 3, 4]
+  assert merge_sort(input) == expected
 
-    self.assertEqual(merge_sort(input), expected)
+def test_numeric_with_odd_number_of_values():
+  input = [2, 1, 4]
+  expected = [1, 2, 4]
 
-  def test_numeric_with_odd_number_of_values(self):
-    input = [2, 1, 4]
-    expected = [1, 2, 4]
+  assert merge_sort(input) == expected
 
-    self.assertEqual(merge_sort(input), expected)
-    
-  #-- Lexical Sort -------------------------------------------------------------
-  def test_lexical_with_one_value(self):
-    input = ['a']
-    expected = ['a']
+def test_numeric_generated():
+  input = [random.randrange(1000) for _ in range(0, 10000)]
+  expected = input.copy()
+  expected.sort()
 
-    self.assertEqual(merge_sort(input), expected)
+  assert merge_sort(input) == expected
+  
+#-- Lexical Sort -------------------------------------------------------------
+def test_lexical_with_one_value():
+  input = ['a']
+  expected = ['a']
 
-  def test_lexical_with_two_values(self):
-    input = ['a', 'b']
-    expected = ['a', 'b']
+  assert merge_sort(input) == expected
 
-    self.assertEqual(merge_sort(input), expected)
+def test_lexical_with_two_values():
+  input = ['a', 'b']
+  expected = ['a', 'b']
 
-  def test_lexical_with_two_values_in_reverse_order(self):
-    input = ['b', 'a']
-    expected = ['a', 'b']
+  assert merge_sort(input) == expected
 
-    self.assertEqual(merge_sort(input), expected)
+def test_lexical_with_two_values_in_reverse_order():
+  input = ['b', 'a']
+  expected = ['a', 'b']
 
-  def test_lexical_with_even_number_of_values(self):
-    input = ['b', 'c', 'a', 'd']
-    expected = ['a', 'b', 'c', 'd']
+  assert merge_sort(input) == expected
 
-    self.assertEqual(merge_sort(input), expected)
+def test_lexical_with_even_number_of_values():
+  input = ['b', 'c', 'a', 'd']
+  expected = ['a', 'b', 'c', 'd']
 
-  def test_lexical_with_odd_number_of_values(self):
-    input = ['b', 'a', 'd']
-    expected = ['a', 'b', 'd']
+  assert merge_sort(input) == expected
 
-    self.assertEqual(merge_sort(input), expected)
+def test_lexical_with_odd_number_of_values():
+  input = ['b', 'a', 'd']
+  expected = ['a', 'b', 'd']
 
-  def test_larger(self):
-    input = list('The quick brown fox jumps over the lazy dog')
-    expected = [
-      ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'T', 
-      'a', 'b', 'c', 'd', 'e', 'e', 'e', 'f', 'g', 
-      'h', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 
-      'o', 'o', 'o', 'p', 'q', 'r', 'r', 's', 't', 
-      'u', 'u', 'v', 'w', 'x', 'y', 'z'
-    ]
+  assert merge_sort(input) == expected
 
-    self.assertEqual(merge_sort(input), expected)
+def test_larger():
+  input = list('The quick brown fox jumps over the lazy dog')
+  expected = [
+    ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'T', 
+    'a', 'b', 'c', 'd', 'e', 'e', 'e', 'f', 'g', 
+    'h', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 
+    'o', 'o', 'o', 'p', 'q', 'r', 'r', 's', 't', 
+    'u', 'u', 'v', 'w', 'x', 'y', 'z'
+  ]
 
-  def test_generated(self):
-    input = [random.choice(string.ascii_letters) for _ in range(0, 10000)]
-    expected = input.copy()
-    expected.sort()
+  assert merge_sort(input) == expected
 
-    self.assertEqual(merge_sort(input), expected)
+def test_lexical_generated():
+  input = [random.choice(string.ascii_letters) for _ in range(0, 10000)]
+  expected = input.copy()
+  expected.sort()
 
-if __name__ == '__main__':
-    unittest.main()
+  assert merge_sort(input) == expected
